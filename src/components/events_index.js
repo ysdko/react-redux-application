026844894a4340625085bf3c8385ca39719ecd91
ysdko <koki.yoshida110@gmail.com>
2020-03-13 15:17:03 +0900

    implement event new component

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import _ from 'lodash'
+import {Link} from 'react-router-dom'
 
 import { readEvents } from '../actions'
 
@@ -25,6 +26,7 @@ class EventsIndex extends Component {
       // <React.Fragment>
       //   <div>{console.log(props.events)}</div>
       // </React.Fragment>
+      <React.Fragment>
       <table>
         <thead>
           <tr>
@@ -38,6 +40,9 @@ class EventsIndex extends Component {
           {this.renderEvents()}
         </tbody>
       </table>
+
+      <Link to="/events/new">New Event</Link>
+      </ React.Fragment>
     )
   }
 }
---
+++ src/components/events_new.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux'
+// import {Field, reduxForm} from 'redux-form'
+import {Link} from 'react-router-dom'
+
+// import { postEvent } from '../actions'
+
+class EventsNew extends Component {
+  
+  render() {
+    return (
+      <React.Fragment>
+        <div>foo</div>
+      </ React.Fragment>
+    )
+  }
+}
+
+
+// const mapDispatchToProps = ({postEvents})
+  
+export default connect(null,null)(EventsNew)
